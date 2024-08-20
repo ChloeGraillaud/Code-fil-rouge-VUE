536bb62ef80c6df3edc16aba791adf9e2a26544d
ChloeGraillaud <cgraillaud@laposte.net>
2024-08-20 16:37:11 +0200

    WORK FOR FUCK SAKE

--- Fil-rouge-chloe/vite.config.js
+++ Fil-rouge-chloe/vite.config.js
@@ -1,20 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dynamicImport from 'vite-plugin-dynamic-import'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
-    plugins: [
-       vue(),
-     dynamicImport(),
-     ],
-    resolve: {
-     alias: {
-       '@': fileURLToPath(new URL('./src', import.meta.url)),
-       '~bootstrap':'bootstrap',
-     }
-     }
-  })
+  base: './',
+  plugins: [
+    vue(),
+    dynamicImport(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap': 'bootstrap',
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['emailjs-com'],
+    },
+  },
+})
+
