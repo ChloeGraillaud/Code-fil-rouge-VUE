22bd6e9ecd37c55a65003b2fb8e90b9066145834
ChloeGraillaud <cgraillaud@laposte.net>
2024-08-20 17:18:49 +0200

    Please

--- Fil-rouge-chloe/vite.config.js
+++ Fil-rouge-chloe/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import dynamicImport from 'vite-plugin-dynamic-import'
 
 export default defineConfig({
-  base: '/',
+  base: '/', // Assurez-vous que la base est configurée pour le déploiement
   plugins: [
     vue(),
     dynamicImport(),
@@ -14,6 +14,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '~bootstrap': 'bootstrap',
     },
+  },
+  build: {
+    rollupOptions: {
+      // Si vous avez des dépendances externes, configurez-les ici
+      external: ['emailjs-com'], // Ajoutez d'autres dépendances si nécessaire
+    },
+    outDir: 'dist', // Assurez-vous que la sortie est correctement configurée
   },
 })
 
